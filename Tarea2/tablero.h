eb98862bbff5696868d995073131e877e6f888c2
bluelf45 <52522150+bluelf45@users.noreply.github.com>
2021-10-18 19:03:49 -0300

    Create tablero.h

---
+++ Tarea2/tablero.h
@@ -0,0 +1,8 @@
+typedef struct {
+	int id; //Tipo de casilla
+	int valor; //valor de la casilla, en caso de agregar dinero o de mover al pj
+}casilla;
+
+typedef struct {
+	casilla* mapeo
+}tablero;
