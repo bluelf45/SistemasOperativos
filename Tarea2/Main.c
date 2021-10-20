256b6940d419a8ad671327570caa0b863b75053c
bluelf45 <52522150+bluelf45@users.noreply.github.com>
2021-10-20 19:08:51 -0300

    Update Main.c
    agregue los indices para el tablero

--- Tarea2/Main.c
+++ Tarea2/Main.c
@@ -9,6 +9,27 @@
 static const int NumCasillas = 28;
 static const int DineroInicial = 100;
 
+// INDICES
+// 0 -> START
+// 1, 2, 5, 16 y 18 -> +50
+// 8, 11, 12, 22, 24 -> +75
+// 3, 6, 20, 26 -> -25
+// 9, 10, 19 -> -50
+// 27 -> -75
+// 4 -> BACK 2
+// 13 -> BACK 3
+// 17, 25 -> BACK 4
+// 15 -> FORWARD 3
+// 23 -> FORWARD 5
+// 7, 21 -> JAIL
+// 14 -> FREE
+
+tablero CrearTablero{
+	tablero tab;
+	tab.mapeo = malloc(28*sizeof(casilla));
+	
+}
+
 int main(){
 	return 0;
 }
