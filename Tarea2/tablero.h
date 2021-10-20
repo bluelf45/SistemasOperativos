821de141411fa3f64af5ee370a2567fd5d29df9e
bluelf45 <52522150+bluelf45@users.noreply.github.com>
2021-10-20 19:10:20 -0300

    Update tablero.h

--- Tarea2/tablero.h
+++ Tarea2/tablero.h
@@ -1,5 +1,5 @@
 typedef struct {
-	int id; //Tipo de casilla
+	int id; //Tipo de casilla 0-> start/free, 1 -> Suma, 2-> mover, 3 -> Jail
 	int valor; //valor de la casilla, en caso de agregar dinero o de mover al pj
 }casilla;
 
